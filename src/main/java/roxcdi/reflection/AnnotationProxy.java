2234b64a6ec289de4a7e7c1652cf40734b8ef11d
gracilianomp <gracilianomp@gmail.com>
2016-03-04 05:29:29 -0300

    fix proxy invokes and add cache to proxy instances

--- src/main/java/roxcdi/reflection/AnnotationProxy.java
+++ src/main/java/roxcdi/reflection/AnnotationProxy.java
@@ -4,6 +4,7 @@ import java.lang.annotation.Annotation;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
+import java.util.HashMap;
 
 public class AnnotationProxy {
 
@@ -18,24 +19,41 @@ public class AnnotationProxy {
 		return a ;
 	}
 	
+	@SuppressWarnings("rawtypes")
+	static final private HashMap<Class, Object> annotationsProxy = new HashMap<>() ;
+	
 	@SuppressWarnings("unchecked")
 	static public <T extends Annotation> T asAnnotation(final Class<T> annotationClass) {
+		
+		synchronized (annotationsProxy) {
+			T proxy = (T) annotationsProxy.get(annotationClass) ;
+			if (proxy != null) return proxy ;
+			
+			proxy = createAnnotationProxy(annotationClass) ;
+			
+			annotationsProxy.put(annotationClass, proxy) ;
+			
+			return proxy ;
+		}
+	}
+	
+	@SuppressWarnings("unchecked")
+	static private <T extends Annotation> T createAnnotationProxy(final Class<T> annotationClass) {
 		
 		T instance = (T) Proxy.newProxyInstance(AnnotationProxy.class.getClassLoader(), new Class<?>[] {annotationClass}, new InvocationHandler() {
 			@Override
 			public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-				
 				if ( method.getName().equals("annotationType") ) {
 					return annotationClass ; 
 				}
 				else if ( method.getName().equals("hashCode") ) {
-					return proxy.hashCode() ; 
+					return System.identityHashCode(proxy) ; 
 				}
 				else if ( method.getName().equals("equals") ) {
-					return proxy.equals(args[0]) ; 
+					return proxy == args[0] ;
 				}
 				else if ( method.getName().equals("toString") ) {
-					return annotationClass.getName() ; 
+					return "@"+annotationClass.getName() ; 
 				}
 				
 				throw new UnsupportedOperationException("Can't handler Annotation method: "+ method) ;
