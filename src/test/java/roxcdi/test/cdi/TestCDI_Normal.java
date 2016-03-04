ce7d0993f3c19db73aa8eacc8757764f5a2d5b5d
gracilianomp <gracilianomp@gmail.com>
2016-03-04 01:57:43 -0300

    separate test of property from basic bootstrap test

--- src/test/java/roxcdi/test/cdi/TestCDI_Normal.java
+++ src/test/java/roxcdi/test/cdi/TestCDI_Normal.java
@@ -7,8 +7,6 @@ import org.jboss.weld.environment.se.WeldContainer;
 import org.junit.Assert;
 import org.junit.Test;
 
-import roxcdi.RoxCDI;
-import roxcdi.parameter.PropertyContext;
 import roxcdi.test.cdi.sys.SampleSys;
 
 public class TestCDI_Normal {
@@ -16,26 +14,13 @@ public class TestCDI_Normal {
 	@Test
 	public void testCDI() {
 		
-		String propValue = "hello" ;
-		
-		System.setProperty("roxcdi.test.property", propValue) ;
-		
 		Weld weld = new Weld() ;
 		
 		WeldContainer weldContainer = weld.initialize() ;
 		
-		String propValueOverwrite = "HELLO!!!" ;
-		
-		PropertyContext propertyContext = new PropertyContext("roxcdi.test.property", propValueOverwrite);
-		PropertyContext.setContext(propertyContext) ;
-		
 		Instance<SampleSys> instance = weldContainer.select(SampleSys.class) ;
 		
 		SampleSys sampleSys = instance.get() ;
-		
-		RoxCDI.ensureConstructed(sampleSys) ;
-		
-		PropertyContext.unsetContext(propertyContext);
 		
 		Assert.assertTrue( sampleSys.getCollectionSize() == 20 );
 		
@@ -44,8 +29,6 @@ public class TestCDI_Normal {
 			
 			Assert.assertEquals( 10+i , elem );
 		}
-		
-		sampleSys.checkProperty(propValueOverwrite);
 		
 		weldContainer.shutdown();
 		
